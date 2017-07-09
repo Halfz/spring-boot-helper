ca8f7a4be54a519f34993d05eb93fd0754cd06df
Juyoung Lee <uptownlee89@gmail.com>
2017-07-09 19:01:33 +0900

    NO-ISSUE # add init script

--- run_scouter.sh
+++ run_scouter.sh
@@ -3,7 +3,6 @@ pkill -f 'scouter-server-boot.jar'
 pkill -f 'scouter.host.jar'
 nohup java -Xmx512m -classpath ./scouter/server/scouter-server-boot.jar scouter.boot.Boot ./scouter/server/lib > $HOME/logs/scouter.out &
 nohup java  -classpath ./scouter/agent.host/scouter.host.jar scouter.boot.Boot ./scouter/agent.host/lib > $HOME/logs/scouter-host.out &
-nohup java  -classpath ./scouter/agent.java/scouter.agent.jar scouter.boot.Boot ./scouter/agent.host/lib > $HOME/logs/scouter-host.out &
 sleep 1
 tail -100 $HOME/logs/scouter.out
 tail -100 $HOME/logs/scouter-host.out
