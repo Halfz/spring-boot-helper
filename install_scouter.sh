6a705a35851fc3fab26c713a26928297eb17bf19
uptown <lee.juyoung04@gmail.com>
2017-09-28 10:07:37 +0900

    upgrade scouter version

--- install_scouter.sh
+++ install_scouter.sh
@@ -1,9 +1,9 @@
 #/bin/bash
-if [ ! -f scouter-all-1.7.1.tar.gz ]; then
+if [ ! -f scouter.tar.gz ]; then
   # if git is slow use google drive ...
-  # wget https://github.com/scouter-project/scouter/releases/download/v1.7.1/scouter-all-1.7.1.tar.gz
-  wget -O scouter-all-1.7.1.tar.gz  "https://drive.google.com/uc?export=download&id=0B5XgPksl3sfPbWJDSG9Vejh4ZTg"
-  tar xvzf scouter-all-1.7.1.tar.gz
+  wget -O scouter.tar.gz https://github.com/scouter-project/scouter/releases/download/v1.7.3.1/scouter-all-1.7.3.1.tar.gz
+  #wget -O scouter.tar.gz  "https://drive.google.com/uc?export=download&id=0B5XgPksl3sfPbWJDSG9Vejh4ZTg"
+  tar xvzf scouter.tar.gz
 fi
 mkdir -p $HOME/app-data
 mkdir -p $HOME/app-data/scouterdb
