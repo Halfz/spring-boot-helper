1852e86c73f41f6e89c7080a82209c2a7d0aa30a
uptown <lee.juyoung04@gmail.com>
2017-09-28 10:12:15 +0900

    Add line for creating log script.

--- run_scouter.sh
+++ run_scouter.sh
@@ -1,6 +1,7 @@
 #/bin/bash
 pkill -f 'scouter-server-boot.jar'
 pkill -f 'scouter.host.jar'
+mkdir -p $HOME/logs/
 nohup java -Dscouter.config=scouter.conf -Xmx512m -classpath ./scouter/server/scouter-server-boot.jar scouter.boot.Boot ./scouter/server/lib > $HOME/logs/scouter.out &
 nohup java -Dscouter.config=scouter.conf -classpath ./scouter/agent.host/scouter.host.jar scouter.boot.Boot ./scouter/agent.host/lib > $HOME/logs/scouter-host.out &
 sleep 1
